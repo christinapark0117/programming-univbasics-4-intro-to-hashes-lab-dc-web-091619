dc09a0a33032599f52619f614d425164c7135a9b
Christina Park <christinapark0117@gmail.com>
2019-09-13 17:49:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,11 +24,11 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash[key] = a_value
-  if hash[key]
-  hash[key] += 1
+  hash[:key] = a_value
+  if hash[:key]
+  hash[:key] = 1
 else
-  hash[key] = 1
+  hash[:key] += 1
 end
   
   # given a hash an a key as parameters, return an updated hash
